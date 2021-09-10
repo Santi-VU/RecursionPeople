1c7978d8301e73d86a935d3097aca4a92d157548
Santi-VU <89940984+Santi-VU@users.noreply.github.com>
2021-09-10 12:36:41 +0200

    Add files via upload

---
+++ RecursionPeople/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursionPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1 = "Fernando";
+            string n2 = "Clara";
+
+            List<Tuple<string, string>> find = new List<Tuple<string, string>>();
+            find = Recursive.Saludar(n1, n2, find);
+
+            LeerLista(find);
+        }
+
+        public static void LeerLista(List<Tuple<string, string>> find)
+        {
+            for (int i = find.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{find[i].Item1} --> {find[i].Item2}");
+            }
+        }
+    }
+}
---
+++ RecursionPeople/Recursive.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPeople
+{
+    class Recursive
+    {
+        private static List<Tuple<string, string>> _personas = Setup();
+        private static int _count;
+
+        public static List<Tuple<string, string>> Setup()
+        {
+            List<Tuple<string, string>> personas = new List<Tuple<string, string>>();
+            personas.Add(Tuple.Create("Juan", "Pedro"));
+            personas.Add(Tuple.Create("Juan", "Fernando"));
+            personas.Add(Tuple.Create("Fernando", "Maria"));
+            personas.Add(Tuple.Create("Maria", "Clara"));
+
+            _count = personas.Count - 1;
+
+            return personas;
+        }
+
+        public static List<Tuple<string, string>> Saludar(string n1, string n2, List<Tuple<string, string>> find)
+        {
+            if (_personas[_count].Item2 == n1)
+            {
+                return find;
+            }
+
+            if (_personas[_count].Item2 == n2)
+            {
+                find.Add(_personas[_count]);
+                n2 = _personas[_count].Item1;
+                _count--;
+                return Saludar(n1, n2, find);
+            }
+
+            return null;
+        }
+    }
+}
